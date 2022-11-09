f54c2a10348da7c7cda9e9bd9f12ceb81657d189
tarun-upadhyay <tarunu88@gmail.com>
2022-11-09 09:14:26 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Footer from './Components/Footer'
+import Navbar from './Components/Navbar'
+import MainRoutes from './Pages/MainRoutes'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <MainRoutes></MainRoutes>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
