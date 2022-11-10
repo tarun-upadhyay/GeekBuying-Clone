d2de675cdbbb903e4d56d65990e6c22c56f5285e
tarun-upadhyay <tarunu88@gmail.com>
2022-11-10 20:26:48 +0530

    products

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Product = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((store)=> store.products)
+  console.log(data)
   return (
     <div>Product</div>
   )
