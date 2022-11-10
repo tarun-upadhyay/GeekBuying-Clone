c885c48d5e399a9f77dee56865d977fb453be6d8
sumeetmore1997 <101966263+sumeetmore1997@users.noreply.github.com>
2022-11-10 21:08:46 +0530

    nav

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { addData } from '../Redux/action';
 
 const Product = () => {
   const dispatch = useDispatch();
+  dispatch(addData)
   const data = useSelector((store)=> store.products)
   console.log(data)
   return (
