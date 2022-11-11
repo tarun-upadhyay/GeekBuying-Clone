a8d881cf9c42b1f4dd859769a63d652aa6361650
abhishekyadav <yadavabhishek2723@gmail.com>
2022-11-12 03:10:25 +0530

    product done

--- src/Homepage/Main.jsx
+++ src/Homepage/Main.jsx
@@ -1,18 +1,24 @@
-import { Container, Flex, HStack } from "@chakra-ui/react";
+import { Box, Container, Flex, HStack, Stack, VStack } from "@chakra-ui/react";
 import { useDispatch } from "react-redux";
 import BannerSlider from "./Homecomponents/BannerSlider";
 import Timer from "./Homecomponents/Timer";
 import TopBoxes from "./Homecomponents/TopBoxes";
+import Trendingproduct from "./Homecomponents/Trendingproduct";
 const Main = () => {
   return (
-    <HStack width="100%" margin={"auto"}>
-      <Flex border={"2px"} margin={"auto"}>
+    <Box>
+    <HStack border={"10px"} width={"80%"}>
+      <Flex border={"2px"} width={"100%"} margin={"auto"} my={20}>
         <BannerSlider />
-        <Flex>
+        <VStack>
           <TopBoxes />
-        </Flex>
+        </VStack>
       </Flex>
-      </HStack>
+      <Stack>
+      </Stack>
+    </HStack>
+    <Trendingproduct/>
+    </Box>
   );
 };
 export default Main;
