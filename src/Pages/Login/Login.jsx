7ed61b9fb2ae5d8cf391dbbaa7d51ce7e7b950d9
Anandgupta09070 <vanand09070gu@gmail.com>
2022-11-11 23:25:50 +0530

    signin login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
+import {Alert} from "react-bootstrap";
+import './Signup.css';
+const Login = () => {
+const [emaillogIn,setEmaillogIn]=useState("");
+const [passwordlogIn,setPasswordlogIn]=useState("");
+const [flag,setFlag]=useState(false);
+const [Homepage,setHomePage]=useState(true);
+function handleLogin(e){
+    e.preventDefault();
+    let email=localStorage.getItem("Email").replace(/"/g,"");
+    let password=localStorage.getItem("Password").replace(/"/g,"");
+
+    if(!emaillogIn || !passwordlogIn){
+        setFlag(true);
+        console.log("Empty");
+    }
+    else if(passwordlogIn !==password ||emaillogIn!==email){
+        setFlag(true)
+    }
+    else{
+        setHomePage(!Homepage);
+        setFlag(false)
+    }
+}
+
+return (
+    <div>
+       
+        <form onSubmit={handleLogin}>
+            <h3>Sign In</h3>
+<div className='formInputgroup'>  
+
+                <input type='text' className='formInput'
+                 placeholder='Email address'
+                 onChange={(event)=> setEmaillogIn(event.target.value)} />
+                
+            </div>
+            <div className='formInputgroup'>
+            
+                <input type='password' className='formInput'
+                 placeholder=' Password'
+                 onChange={(event)=> setPasswordlogIn(event.target.value)} /><br/>
+     
+            </div>
+            <Link to="/Cart"><button type="submit" className='s'>Login</button></Link>
+            {flag && (
+               <Alert color ="primary" variant='danger'>
+                  Please Fill Correct Info 
+               </Alert> 
+            )}
+
+            </form>
+    </div>
+  )
+}
+
+export default Login;
--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -3,10 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Cart from "./Cart";
 import Detail from "./Detail";
 import Homepage from "./Homepage";
-import Login from "./Login";
+import Register from "./Login/Signup";
 import Payment from "./Payment";
 import Product from "./Product";
-import Singup from "./Singup";
 import Admin from "../Admin/Admin";
 const MainRoutes = () => {
   return (
@@ -16,8 +15,7 @@ const MainRoutes = () => {
       <Route path="/detail" element={<Detail />}></Route>
       <Route path="/cart" element={<Cart />}></Route>
       <Route path="/payment" element={<Payment />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/singup" element={<Singup />}></Route>
+      <Route path="login"  element={<Register/>}/>
       <Route path="/admin" element={<Admin />}>
         {" "}
       </Route>
