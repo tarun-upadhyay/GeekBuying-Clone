41a69d3a59b3c5e134e40f38c007150ff6b92af6
abhishekyadav <yadavabhishek2723@gmail.com>
2022-11-11 18:45:39 +0530

    box

--- src/Homepage/Main.jsx
+++ src/Homepage/Main.jsx
@@ -1,16 +1,18 @@
-import { Container } from "@chakra-ui/react"
-import { useDispatch } from "react-redux"
-import BannerSlider from "./Homecomponents/BannerSlider"
+import { Container, Flex, HStack } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import BannerSlider from "./Homecomponents/BannerSlider";
 import Timer from "./Homecomponents/Timer";
 import TopBoxes from "./Homecomponents/TopBoxes";
-const Main=()=>{
-   
-    return (
-        <Container>
-            <BannerSlider/>
-            <TopBoxes/>
-            <Timer/>
-        </Container>
-    )
-}
+const Main = () => {
+  return (
+    <HStack width="100%" margin={"auto"}>
+      <Flex border={"2px"} margin={"auto"}>
+        <BannerSlider />
+        <Flex>
+          <TopBoxes />
+        </Flex>
+      </Flex>
+      </HStack>
+  );
+};
 export default Main;
