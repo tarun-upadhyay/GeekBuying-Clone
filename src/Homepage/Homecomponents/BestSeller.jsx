6c04988908a886f8fe70e8996c0bbcf78d6336a5
abhishekyadav <yadavabhishek2723@gmail.com>
2022-11-12 19:17:59 +0530

    half work done

---
+++ src/Homepage/Homecomponents/BestSeller.jsx
@@ -0,0 +1,6 @@
+
+export const BestSeller=()=>{
+    return(
+        <div></div>
+    )
+}
--- src/Homepage/Homecomponents/Trendingproduct.jsx
+++ src/Homepage/Homecomponents/Trendingproduct.jsx
@@ -5,8 +5,10 @@ import { TrendCard } from "./TrendCard";
 const Trendingproduct = () => {
   const data = TrendingProduct;
   return (
-    <Box width={"90%"} flexDirection={"column"} margin={"auto"}>
-      <Heading fontSize={"23px"}>Trending Categories</Heading>
+    <Box width={{ base: "40%", md: "50%", lg: "90%" }} flexDirection={"column"} margin={"auto"} my={10}>
+      <Heading fontSize={{ base: "0.7rem", md: "0.9rem", lg: "1.2rem" }} mb={"3"}>
+        Trending Categories
+      </Heading>
       <Stack flexDirection={["column", "row"]} width={"100%"} gap={"3%"} margin={"auto"}>
         {data.map((item, i) => {
           return <TrendCard key={i + 1} item={item} />;
